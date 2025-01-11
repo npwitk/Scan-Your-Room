433acd1fefbb0278e25745399be49b66658ae924
Nonprawich I <40120130+npwitk@users.noreply.github.com>
2025-01-11 11:14:40 +0700

    Initial Commit

---
+++ Scan-Your-Room/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Scan-Your-Room
+//
+//  Created by Nonprawich I. on 11/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Scan-Your-Room/Scan_Your_RoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scan_Your_RoomApp.swift
+//  Scan-Your-Room
+//
+//  Created by Nonprawich I. on 11/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct Scan_Your_RoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
